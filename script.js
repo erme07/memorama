3724a8bf93fb1e107b1afdec9668b52e907782b7
erme07 <erik.medina07@gmail.com>
2023-09-13 17:14:03 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const cardsArray = Array.from(document.querySelectorAll('.cards__back'));
 const panelCards = document.querySelector('.cards')
+const moves_indicator = document.getElementById('moves');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
 
 let selectedCards = []
-let hits;
+let hits = 0, moves = 0;
 
 const cards = [
   { name: 'onion', image: 'img/onion.png' },
@@ -37,12 +40,34 @@ const shuffle = () => {
   })
 }
 
+const completeGame = () => {
+  alert('ganasteeeeee')
+}
+
+const startTimer = () => {
+  let sec_count = 0, min_count = 0;
+  const timer = setInterval(() => {
+    sec_count++
+    if (sec_count === 60) {
+      sec_count = 0;
+      min_count++
+      if (min_count == 60) min_count = 0;
+      if (min_count < 10) minutes.innerHTML = '0' + min_count;
+      else minutes.innerHTML = min_count;
+    }
+    if (sec_count < 10) seconds.innerHTML = '0' + sec_count;
+    else seconds.innerHTML = sec_count;
+  }, 1000)
+  return timer
+}
+
 const succes = () => {
   hits++;
   selectedCards[0].children[1].classList.add('hits');
   selectedCards[1].children[1].classList.add('hits');
   selectedCards = [];
   panelCards.classList.remove('bloqued');
+  if (hits === 8) completeGame();
 }
 
 const fault = () => {
@@ -56,6 +81,9 @@ const checkSelections = (e) => {
   const count = selectedCards.push(e.target.parentElement);
   e.target.parentElement.classList.add('flip');
   if (count === 2) {
+    moves++
+    if (moves < 10) moves_indicator.innerHTML = '0' + moves;
+    else moves_indicator.innerHTML = moves;
     panelCards.classList.add('bloqued')
     if (selectedCards[0].children[1].getAttribute('data-name') !== selectedCards[1].children[1].getAttribute('data-name')) {
       setTimeout(fault, 700);
