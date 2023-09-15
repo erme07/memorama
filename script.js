48f2773c3c852c5fc43b2155bc2e8cd0fcdce76c
erme07 <erik.medina07@gmail.com>
2023-09-14 23:02:06 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cardsArray = Array.from(document.querySelectorAll('.cards__back'));
 const panelCards = document.querySelector('.cards')
-const fails_indicator = document.getElementById('fails');
+const display_mode = document.getElementById('display-mode');
+
 const cent_seconds = document.querySelector('.cent-seconds');
 const seconds = document.querySelector('.seconds');
 const lives = document.querySelector('.display__chances');
+const display_time = document.querySelector('.display__time');
 const startButton = document.getElementById('start');
 const modesButton = document.getElementById('modes');
 const modal_mode = document.getElementById('modal-mode');
@@ -12,7 +14,7 @@ const mode2_button = document.getElementById('mode2');
 const instruction1 = document.querySelector('.instruction--mode1');
 const instruction2 = document.querySelector('.instruction--mode2');
 const modal_win = document.getElementById('win');
-const play_again = document.getElementById('play-again');
+const modal_lose = document.getElementById('lose');
 
 let selectedCards = []
 let hits = 0, fails = 0, timer, mode = true;
@@ -50,21 +52,54 @@ const shuffle = () => {
   })
 }
 
-const startTimer = () => {
-  let cent_count = 0, seg_count = 0;
-  const timer = setInterval(() => {
-    cent_count++
-    if (cent_count === 100) {
-      cent_count = 0;
-      seg_count++
-      if (seg_count == 60) seg_count = 0;
-      if (seg_count < 10) seconds.innerHTML = '0' + seg_count;
+// const startTimer = () => {
+//   let cent_count = 0, seg_count = 0;
+//   const timer = setInterval(() => {
+//     cent_count++
+//     if (cent_count === 100) {
+//       cent_count = 0;
+//       seg_count++
+//       if (seg_count == 60) seg_count = 0;
+//       if (seg_count < 10) seconds.innerHTML = '0' + seg_count;
+//       else seconds.innerHTML = seg_count;
+//     }
+//     if (cent_count < 10) cent_seconds.innerHTML = '0' + cent_count;
+//     else cent_seconds.innerHTML = cent_count;
+//   }, 10)
+//   return timer
+// }
+
+const countDownTimer = (seg) => {
+  let cent_count = 100, seg_count = seg, aux = seg;
+  const timer_ = setInterval(() => {
+    cent_count--;
+    if (cent_count === 0) {
+      if (seg_count === 0 && cent_count === 0) {
+        cent_seconds.innerHTML = '00';
+        clearInterval(timer_);
+        if (aux !== 9) endGame(false);
+        else {
+          seconds.innerHTML = '29'
+          timer = countDownTimer(29);
+          Array.from(document.querySelectorAll('.cards__place')).forEach((e) => {
+            e.classList.remove('flip');
+          })
+        }
+        return;
+      }
+      cent_count = 100;
+      seg_count--
+      if (seg_count < 10) {
+        seconds.innerHTML = '0' + seg_count
+        if (aux !== 9) display_time.style = 'color:red';
+      }
       else seconds.innerHTML = seg_count;
     }
     if (cent_count < 10) cent_seconds.innerHTML = '0' + cent_count;
     else cent_seconds.innerHTML = cent_count;
+
   }, 10)
-  return timer
+  return timer_
 }
 
 const succes = () => {
@@ -87,11 +122,12 @@ const checkSelections = (e) => {
   const count = selectedCards.push(e.target.parentElement);
   e.target.parentElement.classList.add('flip');
   if (count === 2) {
-    fails++
-    if (fails < 10) fails_indicator.innerHTML = '0' + fails;
-    else fails_indicator.innerHTML = fails;
     panelCards.classList.add('bloqued')
     if (selectedCards[0].children[1].getAttribute('data-name') !== selectedCards[1].children[1].getAttribute('data-name')) {
+      fails++
+      if (fails === 3) {
+        setTimeout(endGame, 400, false)
+      }
       setTimeout(fault, 700);
     } else {
       setTimeout(succes, 400);
@@ -99,22 +135,41 @@ const checkSelections = (e) => {
   }
 }
 
+const modeOne = () => {
+  seconds.innerHTML = '39'
+  timer = countDownTimer(39);
+}
+
+const modeTwo = () => {
+  const cards = Array.from(document.querySelectorAll('.cards__place'));
+  cards.forEach((e) => {
+    e.classList.add('flip')
+  })
+  seconds.innerHTML = '09'
+  timer = countDownTimer(9);
+}
+
 const startGame = () => {
   panelCards.classList.remove('bloqued');
   startButton.classList.add('disabled');
   modesButton.classList.add('disabled');
-  lives.classList.add('disabled');
   panelCards.classList.remove('shuffle');
-  timer = startTimer();
+  if (mode) {
+    lives.classList.add('disabled');
+    modeOne();
+  }
+  else modeTwo();
 }
 
-const endGame = () => {
+const endGame = (condition) => {
   panelCards.classList.add('bloqued');
   startButton.classList.remove('disabled');
   modesButton.classList.remove('disabled');
   lives.classList.remove('disabled');
   clearInterval(timer);
-  modal_win.classList.remove('hidden');
+  if (condition) modal_win.classList.remove('hidden');
+  else modal_lose.classList.remove('hidden');
+
 }
 
 const changeMode = (e) => {
@@ -122,8 +177,14 @@ const changeMode = (e) => {
   mode1_button.classList.toggle('unselected');
   instruction1.classList.toggle('show');
   instruction2.classList.toggle('show');
-  if (e.target.matches('#mode1')) mode = true;
-  else mode = false;
+  if (e.target.matches('#mode1')) {
+    mode = true;
+    seconds.innerHTML = '40';
+  }
+  else {
+    seconds.innerHTML = '10';
+    mode = false;
+  }
 }
 
 window.addEventListener('load', () => {
@@ -139,11 +200,13 @@ document.addEventListener('click', (e) => {
   if (e.target.matches('#start')) {
     startGame()
   }
-  if (e.target.matches('#play-again')) {
+  if (e.target.matches('.button--rematch')) {
     modal_win.classList.add('hidden')
-    fails_indicator.innerHTML = '00';
-    minutes.innerHTML = '00';
-    seconds.innerHTML = '00';
+    modal_lose.classList.add('hidden')
+    if (mode) seconds.innerHTML = '40';
+    else seconds.innerHTML = '10';
+    cent_seconds.innerHTML = '00';
+    display_time.style = '';
     Array.from(document.querySelectorAll('.flip')).forEach((e) => {
       e.classList.remove('flip');
     })
@@ -164,8 +227,14 @@ document.addEventListener('click', (e) => {
 
   if (e.target.matches('#save-mode')) {
     modal_mode.classList.add("hidden");
-    if (mode) modesButton.innerHTML = 'mode 1';
-    else modesButton.innerHTML = 'mode 2';
+    if (mode) {
+      lives.classList.add('disabled');
+      display_mode.innerHTML = 'mode 1';
+    }
+    else {
+      lives.classList.remove('disabled');
+      display_mode.innerHTML = 'mode 2';
+    }
 
   }
 })
